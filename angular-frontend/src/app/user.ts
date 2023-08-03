e3c50e31c49dabd1b084e9560fbdad3ed3af95be
ZAGHDOUDI_Mohamed <mohamed.zgh28@gmail.com>
2023-08-03 10:56:00 +0100

    first commit

---
+++ angular-frontend/src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+    id: number = 0; // Valeur par défaut (vous pouvez utiliser une autre valeur)
+    firstName: string = '';
+    lastName: string = '';
+    email: string = '';
+}
